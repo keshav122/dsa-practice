75ba5e1ffa1ec38729fe8021b13e83ff430a9571
Keshav <keshavchaudhary401@gmail.com>
2024-07-07 22:36:47 +0530

    LC_Problems

---
+++ LeetCode_Practice/src/com/problems/easy/WaterBottles_1518.java
@@ -0,0 +1,15 @@
+package com.problems.easy;
+
+public class WaterBottles_1518 {
+
+	public int numWaterBottles(int numBottles, int numExchange) {
+
+		int count = numBottles;
+		int empyWaterBottles = numBottles;
+		while(empyWaterBottles >= numExchange) {
+			count += (empyWaterBottles/numExchange);
+			empyWaterBottles = (empyWaterBottles/numExchange) + (empyWaterBottles % numExchange);
+		}
+		return count;
+	}
+}
