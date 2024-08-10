3f455a4dde5c46985046cb2682989bf9b2339216
Keshav <keshavchaudhary401@gmail.com>
2024-08-11 01:14:36 +0530

    TUF+ Roadmap Day 1-3 practice - Beginner Problems

---
+++ DSA_Series/src/com/roadmap/patterns/Pattern1.java
@@ -0,0 +1,13 @@
+package com.roadmap.patterns;
+
+public class Pattern1 {
+    public void pattern1(int n) {
+        // The pattern is a n*n square of asterisks
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
