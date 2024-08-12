12640c67948c6f193dd3336e12e2ee66a22de4af
Keshav <keshavchaudhary401@gmail.com>
2024-08-12 18:40:32 +0530

    Pattern problems : Medium and Hard

---
+++ DSA_Series/src/com/roadmap/patterns/Pattern21.java
@@ -0,0 +1,23 @@
+package com.roadmap.patterns;
+
+public class Pattern21 {
+    public void pattern21(int n) {
+        for (int row = 1; row <= n; row++) {
+            if (row == 1 || row == n) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                // Print one asterisk
+                System.out.print("*");
+                // Print n-2 spaces
+                for (int j = 1; j < n - 1; j++) {
+                    System.out.print(" ");
+                }
+                // Print one asterisk
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
