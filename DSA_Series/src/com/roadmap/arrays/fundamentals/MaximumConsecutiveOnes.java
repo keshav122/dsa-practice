380dfa83b8589b347ac01ed807d1222dac6ca829
Keshav <keshavchaudhary401@gmail.com>
2024-08-20 01:23:51 +0530

    Array Fundamentals

---
+++ DSA_Series/src/com/roadmap/arrays/fundamentals/MaximumConsecutiveOnes.java
@@ -0,0 +1,17 @@
+package com.roadmap.arrays.fundamentals;
+
+public class MaximumConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveOnes = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                maxConsecutiveOnes = Math.max(maxConsecutiveOnes, count);
+            } else {
+                count = 0;
+            }
+        }
+        return maxConsecutiveOnes;
+    }
+}
