25fb24f38e61416affeb8b92e57d1de32156a332
Keshav <keshavchaudhary401@gmail.com>
2025-01-27 00:57:29 +0530

    Greeedy Algo Problem_27_01_2024

---
+++ DSA_Series/src/com/roadmap/greedy_algorithms/scheduling_and_interval_problems/ShortestJobFirst.java
@@ -0,0 +1,16 @@
+package com.roadmap.greedy_algorithms.scheduling_and_interval_problems;
+
+import java.util.Arrays;
+
+public class ShortestJobFirst {
+    public long solve(int[] bt) {
+        Arrays.sort(bt);
+        int n = bt.length;
+        long ans = 0;
+        for (int i = 1; i < n; i++) {
+            bt[i] += bt[i - 1];
+            ans += bt[i - 1];
+        }
+        return ans / n;
+    }
+}
