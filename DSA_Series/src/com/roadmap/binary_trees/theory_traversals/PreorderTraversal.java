5be9c13f3db098635b0fcc81cf5c66340c9a8e74
Keshav <keshavchaudhary401@gmail.com>
2025-02-28 12:59:11 +0530

    Preorder_Iterative

--- DSA_Series/src/com/roadmap/binary_trees/theory_traversals/PreorderTraversal.java
+++ DSA_Series/src/com/roadmap/binary_trees/theory_traversals/PreorderTraversal.java
@@ -2,6 +2,7 @@ package com.roadmap.binary_trees.theory_traversals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 //Root-> Left-> Right
 public class PreorderTraversal {
@@ -18,5 +19,22 @@ public class PreorderTraversal {
         li.add(root.data);
         preorder(root.left, li);
         preorder(root.right, li);
+    }
+
+    public List<Integer> preorderIterative(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            TreeNode top = st.pop();
+            res.add(top.data);
+            if (top.right != null) {
+                st.add(top.right);
+            }
+            if (top.left != null) {
+                st.add(top.left);
+            }
+        }
+        return res;
     }
 }
