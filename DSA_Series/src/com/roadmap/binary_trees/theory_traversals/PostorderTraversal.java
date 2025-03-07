5f5e4f064b14b3a20f51f56d5d74d5dc566e1521
Keshav <keshavchaudhary401@gmail.com>
2025-03-07 14:53:24 +0530

    TreeTraversals

--- DSA_Series/src/com/roadmap/binary_trees/theory_traversals/PostorderTraversal.java
+++ DSA_Series/src/com/roadmap/binary_trees/theory_traversals/PostorderTraversal.java
@@ -1,7 +1,9 @@
 package com.roadmap.binary_trees.theory_traversals;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class PostorderTraversal {
     public List<Integer> postorder(TreeNode root) {
@@ -16,5 +18,23 @@ public class PostorderTraversal {
         postorder(root.left, list);
         postorder(root.right, list);
         list.add(root.data);
+    }
+
+    public List<Integer> postorder_Iterative(TreeNode root) {
+        // We will have the root -> right-> left and then reverse the result
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+
+        while (!st.isEmpty()) {
+            TreeNode node = st.pop();
+            list.add(node.data);
+            if (node.left != null)
+                st.push(node.left);
+            if (node.right != null)
+                st.push(node.right);
+        }
+        Collections.reverse(list);
+        return list;
     }
 }
