a9d2d0d1a11b193a57890e6c35c509b530bf309a
Keshav <keshavchaudhary401@gmail.com>
2025-03-18 15:57:13 +0530

    Binary Trees FAQs Problems

---
+++ LeetCode_Practice/src/com/problems/easy/MaxCountOfPosAndNegativeInteger_2529.java
@@ -0,0 +1,28 @@
+package com.problems.easy;
+
+public class MaxCountOfPosAndNegativeInteger_2529 {
+    public int maximumCount(int[] nums) {
+        int negCount = 0;
+        int i = 0;
+        int n = nums.length;
+        for (; i < n; i++) {
+            if (nums[i] == 0)
+                continue;
+            else if (nums[i] < 0)
+                negCount++;
+            else
+                break;
+        }
+        int posCount = n - i;
+        return posCount > negCount ? posCount : negCount;
+    }
+
+    public int maximumCount_better(int[] nums) {
+        // Calculate the lower bound and upper bound for zero and based on that we can
+        // calculate the
+        // max value.
+
+        // TODO Try the better solution
+        return 0;
+    }
+}
