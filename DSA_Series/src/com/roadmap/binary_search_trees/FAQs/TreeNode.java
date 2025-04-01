21ed1116f16186145b3f411e2bd303670e164243
Keshav <keshavchaudhary401@gmail.com>
2025-04-01 20:45:36 +0530

    BST Medium Problems

---
+++ DSA_Series/src/com/roadmap/binary_search_trees/FAQs/TreeNode.java
@@ -0,0 +1,13 @@
+package com.roadmap.binary_search_trees.FAQs;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
--- DSA_Series/src/com/roadmap/binary_search_trees/medium_problems/ConstructABSTusingPreorder.java
+++ DSA_Series/src/com/roadmap/binary_search_trees/medium_problems/ConstructABSTusingPreorder.java
@@ -1,6 +1,66 @@
 package com.roadmap.binary_search_trees.medium_problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class ConstructABSTusingPreorder {
+    // In this approach we iterate through each of the nodes and place it at its
+    // correct position
+    public TreeNode bstFromPreorder_BF(int[] preorder) {
+        if (preorder.length == 0)
+            return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        for (int i = 1; i < preorder.length; i++) {
+            TreeNode node = st.peek();
+            TreeNode child = new TreeNode(preorder[i]);
+            while (!st.isEmpty() && st.peek().data < preorder[i]) {
+                node = st.pop();
+            }
+            if (node.data < preorder[i]) {
+                node.right = child;
+            } else {
+                node.left = child;
+            }
+
+            st.push(child);
+        }
+        return root;
+    }
+
+    // In this approach we create a inorder array as we know that inorder array of
+    // BST is sorted in non-decreasing fashion.
+    // Then we can use the inorder and preorder to build BST as we had done in
+    // Binary trees
+    public TreeNode bstFromPreorder_Better(int[] preorder) {
+        int n = preorder.length;
+        int[] inorder = new int[n];
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            inorder[i] = preorder[i];
+        }
+        Arrays.sort(inorder);
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return buildTree(preorder, 0, n - 1, inorder, 0, n - 1, indexMap);
+    }
+
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd,
+            Map<Integer, Integer> indexMap) {
+        if (preStart > preEnd || inStart > inEnd)
+            return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int rootIndex = indexMap.get(root.data);
+        int leftCount = rootIndex - inStart;
+        root.left = buildTree(preorder, preStart + 1, preStart + leftCount, inorder, inStart, rootIndex - 1, indexMap);
+        root.right = buildTree(preorder, preStart + leftCount + 1, preEnd, inorder, rootIndex + 1, inEnd, indexMap);
+        return root;
+    }
+
     public TreeNode bstFromPreorder(int[] preorder) {
         return bstFromPreorder(preorder, 0, preorder.length - 1);
     }
