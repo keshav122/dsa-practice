e97e0b2134bcd53c8d6fe0b180f62c1a26bfcdfc
Keshav <keshavchaudhary401@gmail.com>
2025-07-01 04:12:16 +0530

    Binary Search Revision , LC Practice and Graphs

--- src/com/dsa_series/roadmap/binary_search/fundamentals/SearchXInSortedArray.java
+++ src/com/dsa_series/roadmap/binary_search/fundamentals/SearchXInSortedArray.java
@@ -3,7 +3,7 @@ package com.dsa_series.roadmap.binary_search.fundamentals;
 /*Author: keshav122 */
 
 public class SearchXInSortedArray {
-    public int search(int[] nums, int target) {
+    public int binarysearch(int[] nums, int target) {
         int n = nums.length;
         int start = 0;
         int end = n - 1;
@@ -17,5 +17,23 @@ public class SearchXInSortedArray {
                 start = mid + 1;
         }
         return -1;
+    }
+
+    // Recursive Solution
+    public int search(int[] nums, int target) {
+        return search(nums, 0, nums.length - 1, target);
+    }
+
+    private int search(int[] nums, int start, int end, int target) {
+        if (start > end)
+            return -1;
+        int mid = start + ((end - start) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return search(nums, start, mid - 1, target);
+        } else {
+            return search(nums, mid + 1, end, target);
+        }
     }
 }
