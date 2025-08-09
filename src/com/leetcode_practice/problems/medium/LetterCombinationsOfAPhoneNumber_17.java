d3c917ca0680d700a9584c6240a3dbf895432c4c
Keshav <keshavchaudhary401@gmail.com>
2025-08-09 21:01:59 +0530

    LC Problems and DP

---
+++ src/com/leetcode_practice/problems/medium/LetterCombinationsOfAPhoneNumber_17.java
@@ -0,0 +1,26 @@
+package com.leetcode_practice.problems.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationsOfAPhoneNumber_17 {
+    public List<String> letterCombinations(String digits) {
+        String[] combs = new String[] { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        List<String> letters = new ArrayList<>();
+        if (digits.length() == 0)
+            return letters;
+        letterCombinations(0, "", digits, letters, combs);
+        return letters;
+    }
+
+    private void letterCombinations(int index, String currString, String digits, List<String> letters, String[] combs) {
+        if (index == digits.length()) {
+            letters.add(currString);
+            return;
+        }
+        String keypad = combs[digits.charAt(index) - '0'];
+        for (int i = 0; i < keypad.length(); i++) {
+            letterCombinations(index + 1, currString + keypad.charAt(i), digits, letters, combs);
+        }
+    }
+}
