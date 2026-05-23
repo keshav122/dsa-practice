81e39c0498f432d3a48fbe4d359c5933a8b6e35d
Keshav <keshavchaudhary401@gmail.com>
2026-05-23 23:56:12 +0530

    LC Practice

--- src/com/leetcode_practice/problems/easy/CheckIfArrayIsSortedAndRotated_1752.java
+++ src/com/leetcode_practice/problems/easy/CheckIfArrayIsSortedAndRotated_1752.java
@@ -1,24 +1,37 @@
 package com.leetcode_practice.problems.easy;
 
-
 //Note: An array A rotated by x positions results in an array B of the same 
 //length such that A[i] == B[(i+x) % A.length],
 //where % is the modulo operation.
 
 //Use Case to look at : [2,1,3,4]
-public class CheckIfArrayIsSortedAndRotated_1752{
+public class CheckIfArrayIsSortedAndRotated_1752 {
 
-	public boolean check(int[] nums) {
+    public boolean check(int[] nums) {
         int count = 0;
         int n = nums.length;
-        for(int i = 1; i < n;i++) {
-        	if(nums[i] < nums[i-1]) {
-        		count++;
-        	}
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
         }
-        
-        if(nums[n-1] > nums[0]) count++;//There should be max one incident where the higher index element is smaller than
-        //lower index element in the circular chain.
-       return count<= 1 ? true : false;
+
+        if (nums[n - 1] > nums[0])
+            count++;// There should be max one incident where the higher index element is smaller
+                    // than
+        // lower index element in the circular chain.
+        return count <= 1 ? true : false;
+    }
+
+    // We can treat the array as circular
+    public boolean check_betters(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[(i + 1 % n)]) {
+                count++;
+            }
+        }
+        return count <= 1;
     }
 }
